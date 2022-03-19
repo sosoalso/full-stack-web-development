5bb2ab793a12637040a9c2e562d04de1c9c57580
sosoalso <sosoalso@gmail.com>
2022-03-19 09:27:06 +0000

    Develop HTML structure.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import TodoItem from "$lib/todo-item.svelte";
+
+  const title = "TODO임ㅋ";
+</script>
+
+<svelte:head><title>{title}</title></svelte:head>
+<h1>{title}</h1>
+
+<form action="" method="">
+  <input
+    type="text"
+    name=""
+    id=""
+    aria-label="Add a todo"
+    placeholder="+ tap to add todo"
+  />
+</form>
+
+<TodoItem />
